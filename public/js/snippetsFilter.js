b328bf2eade4f89f4f9729a58c084f5ef305d253
Jim Disenstam <jim.disenstam@gmail.com>
2019-02-26 15:47:50 +0100

    Reworking snippet filters

--- public/js/snippetsFilter.js
+++ public/js/snippetsFilter.js
@@ -1,25 +1,47 @@
 /* global $ */
 'use strict'
 
-$('#filterLanguageList').on('change', (event) => {
-  let selected = event.currentTarget.value.toLowerCase()
-  if (selected === 'language') {
-    $('tbody tr').show()
-  } else $('tbody tr').hide()
-  $('tbody tr').each((i, el) => {
-    let language = $(el).data('language')
-      ? $(el).data('language').toLowerCase()
-      : null
-    if (language === selected) $(el).show()
-  })
-})
+function populateOptions (selection) {
+  const languageList = $('#filterLanguageList')
+  const authorList = $('#filterAuthorList')
+  let languages = [...new Set(
+    $('.language-td')
+      .map((i, el) => {
+        if ($(el).text().length > 0) return $(el).text()
+      })
+  )]
+  languages.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()))
+  languages.forEach(language => languageList.append(`<option>${language}</option>)`))
 
-$('#filterAuthorList').on('change', (event) => {
-  let selected = event.currentTarget.value
-  if (selected.toLowerCase() === 'author') {
-    $('tbody tr').show()
-  } else $('tbody tr').hide()
+  let authors = [...new Set($('.author-td').map((i, el) => $(el).text()))]
+  authors.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()))
+  authors.forEach(author => authorList.append(`<option>${author}</option>)`))
+}
+
+function filterList () {
+  $('tbody tr').hide()
+  let noAuthor = $('#filterAuthorList > option').first().val()
+  let noLanguage = $('#filterLanguageList > option').first().val()
+  let author = $('#filterAuthorList').val()
+  let language = $('#filterLanguageList').val()
   $('tbody tr').each((i, el) => {
-    if ($(el).data('author') === selected) $(el).show()
+    if ($(el).data('language') === language) $(el).show()
+    if ($(el).data('author') === author) $(el).show()
   })
-})
+  // $('tbody tr').each((i, el) => {
+  //   let rowLanguage = $(el).data('language')
+  //     ? $(el).data('language').toLowerCase()
+  //     : null
+  //   let rowAuthor = $(el).data('author')
+  //   if (rowLanguage === filter.language || filter.language.toLowerCase() === 'language') {
+  //     console.log(rowAuthor)
+  //     if (rowAuthor === filter.author || filter.author.toLowerCase() === 'author') $(el).show()
+  //   }
+  // })
+}
+
+$('#filterLanguageList').on('change', event => filterList())
+
+$('#filterAuthorList').on('change', event => filterList())
+
+populateOptions()
